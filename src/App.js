94bf3295c9cb56b31159031d0b8b19ba1479d383
toniemati <matikas2000@gmail.com>
2021-09-08 20:42:47 +0200

    cleanup project, and fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      const { data } = await axios.get('https://disease.sh/v3/covid-19/countries');
+      setCountries(data);
+    }
+    getCountries();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>covid-19 tracker 💙</h1>
+
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="PL">
+            {countries.map((country) => (
+              <MenuItem key={country.countryInfo.iso2} value={country.countryInfo.iso2}>{country.country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+        {/* title + select input */}
+        
+      {/* infoboxes */}
+      {/* infoboxes */}
+      {/* infoboxes */}
+
+      {/* table */}
+      {/* graph */}
+
+      {/* map */}
     </div>
   );
 }
